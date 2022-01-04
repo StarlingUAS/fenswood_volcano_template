a211f675144ae40988ca18aabaee46afc21f5c49
Arthur <arthur.richards@bristol.ac.uk>
2022-01-04 19:23:28 +0000

    Control now implemented using a ROS2 timer.

--- fenswood_drone_controller/fenswood_drone_controller/controller.py
+++ fenswood_drone_controller/fenswood_drone_controller/controller.py
@@ -19,30 +19,22 @@ class FenswoodDroneController(Node):
 
     def __init__(self):
         super().__init__('example_controller')
-        self.last_status = None     # global for last received status message
-        self.last_pos = None       # global for last received position message
-        self.init_alt = None       # global for global altitude at start
-        self.last_alt_rel = None   # global for last altitude relative to start
+        self.last_status = None     # store for last received status message
+        self.last_pos = None       # store for last received position message
+        self.init_alt = None       # store for global altitude at start
+        self.last_alt_rel = None   # store for last altitude relative to start
         # set up two subscribers, one for vehicle state...
         state_sub = self.create_subscription(State, 'mavros/state', self.state_callback, 10)
         # ...and the other for global position
         pos_sub = self.create_subscription(NavSatFix, 'mavros/global_position/global', self.position_callback, 10)
         # create service clients for long command (datastream requests)...
         self.cmd_cli = self.create_client(CommandLong, 'mavros/cmd/command')
-        while not self.cmd_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('command_int service not available, waiting again...')
         # ... for mode changes ...
         self.mode_cli = self.create_client(SetMode, 'mavros/set_mode')
-        while not self.mode_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('set_mode service not available, waiting again...')
         # ... for arming ...
         self.arm_cli = self.create_client(CommandBool, 'mavros/cmd/arming')
-        while not self.arm_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('arming service not available, waiting again...')
         # ... and for takeoff
         self.takeoff_cli = self.create_client(CommandTOL, 'mavros/cmd/takeoff')
-        while not self.takeoff_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('takeoff service not available, waiting again...')
         # create publisher for setpoint
         self.target_pub = self.create_publisher(GeoPoseStamped, 'mavros/setpoint_position/global', 10)
         # and make a placeholder for the last sent target
@@ -51,7 +43,9 @@ class FenswoodDroneController(Node):
         self.control_state = 'init'
         # timer for time spent in each state
         self.state_timer = 0
-        
+        # create a ROS2 timer to run the control actions
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
     # on receiving status message, save it to global
     def state_callback(self,msg):
         self.last_status = msg
@@ -67,50 +61,31 @@ class FenswoodDroneController(Node):
                                                                         msg.longitude,
                                                                         self.last_alt_rel))
 
-    def wait_for_new_status(self):
-        """
-        Wait for new state message to be received.  These are sent at
-        1Hz so calling this is roughly equivalent to one second delay.
-        """
-        if self.last_status:
-            # if had a message before, wait for higher timestamp
-            last_stamp = self.last_status.header.stamp.sec
-            for try_wait in range(60):
-                rclpy.spin_once(self)
-                if self.last_status.header.stamp.sec > last_stamp:
-                    break
-        else:
-            # if never had a message, just wait for first one          
-            for try_wait in range(60):
-                if self.last_status:
-                    break
-                rclpy.spin_once(self)
-
     def request_data_stream(self,msg_id,msg_interval):
         cmd_req = CommandLong.Request()
         cmd_req.command = 511
         cmd_req.param1 = float(msg_id)
         cmd_req.param2 = float(msg_interval)
         future = self.cmd_cli.call_async(cmd_req)
-        rclpy.spin_until_future_complete(self, future)    # wait for response
+        self.get_logger().info('Requested msg {} every {} us'.format(msg_id,msg_interval))
 
     def change_mode(self,new_mode):
         mode_req = SetMode.Request()
         mode_req.custom_mode = new_mode
         future = self.mode_cli.call_async(mode_req)
-        rclpy.spin_until_future_complete(self, future)    # wait for response
+        self.get_logger().info('Request sent for {} mode.'.format(new_mode))
 
     def arm_request(self):
         arm_req = CommandBool.Request()
         arm_req.value = True
         future = self.arm_cli.call_async(arm_req)
-        rclpy.spin_until_future_complete(self, future)
+        self.get_logger().info('Arm request sent')
 
     def takeoff(self,target_alt):
         takeoff_req = CommandTOL.Request()
         takeoff_req.altitude = target_alt
         future = self.takeoff_cli.call_async(takeoff_req)
-        rclpy.spin_until_future_complete(self, future)
+        self.get_logger().info('Requested takeoff to {}m'.format(target_alt))
 
     def flyto(self,lat,lon,alt):
         self.last_target.pose.position.latitude = lat
@@ -121,16 +96,17 @@ class FenswoodDroneController(Node):
 
     def state_transition(self):
         if self.control_state =='init':
-            if self.last_status.system_status==3:
-                self.get_logger().info('Drone initialized')
-                # send command to request regular position updates
-                self.request_data_stream(33, 1000000)
-                self.get_logger().info('Requested position stream')
-                # change mode to GUIDED
-                self.change_mode("GUIDED")
-                self.get_logger().info('Request sent for GUIDED mode.')
-                # move on to arming
-                return('arming')
+            if self.last_status:
+                if self.last_status.system_status==3:
+                    self.get_logger().info('Drone initialized')
+                    # send command to request regular position updates
+                    self.request_data_stream(33, 1000000)
+                    # change mode to GUIDED
+                    self.change_mode("GUIDED")
+                    # move on to arming
+                    return('arming')
+                else:
+                    return('init')
             else:
                 return('init')
 
@@ -142,7 +118,6 @@ class FenswoodDroneController(Node):
                     self.init_alt = self.last_pos.altitude
                 # send takeoff command
                 self.takeoff(20.0)
-                self.get_logger().info('Takeoff request sent.')
                 return('climbing')
             elif self.state_timer > 60:
                 # timeout
@@ -150,7 +125,6 @@ class FenswoodDroneController(Node):
                 return('exit')
             else:
                 self.arm_request()
-                self.get_logger().info('Arming request sent.')
                 return('arming')
 
         elif self.control_state == 'climbing':
@@ -184,20 +158,20 @@ class FenswoodDroneController(Node):
         elif self.control_state == 'landing':
             # return home and land
             self.change_mode("RTL")
-            self.get_logger().info('Request sent for RTL mode.')
             return('exit')
 
+        elif self.control_state == 'exit':
+            # nothing else to do
+            return('exit')
 
-    def run(self):
-        for try_loop in range(600):
-            if rclpy.ok():
-                self.wait_for_new_status()
-                new_state = self.state_transition()
-                if new_state == self.control_state:
-                    self.state_timer = self.state_timer + 1
-                else:
-                    self.state_timer = 0
-                self.control_state = new_state
+    def timer_callback(self):
+        new_state = self.state_transition()
+        if new_state == self.control_state:
+            self.state_timer = self.state_timer + 1
+        else:
+            self.state_timer = 0
+        self.control_state = new_state
+        self.get_logger().info('Controller state: {} for {} steps'.format(self.control_state, self.state_timer))
                     
 
 def main(args=None):
@@ -205,7 +179,7 @@ def main(args=None):
     rclpy.init(args=args)
 
     controller_node = FenswoodDroneController()
-    controller_node.run()
+    rclpy.spin(controller_node)
 
 
 if __name__ == '__main__':
